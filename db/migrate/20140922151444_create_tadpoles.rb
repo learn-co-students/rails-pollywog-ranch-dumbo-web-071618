5751bc5cc838a76810cf9c45751eb75d8f868686
Jason Pitts <juniorjr083@gmail.com>
2018-08-19 20:22:16 +0000

    Done.

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,6 @@
 class CreateTadpoles < ActiveRecord::Migration
   # code goes here
+  add_column :tadpoles, :name, :string 
+  add_column :tadpoles, :color, :string 
+  add_column :tadpoles, :frog_id, :integer
 end
--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,3 +1,5 @@
 class CreatePonds < ActiveRecord::Migration
   # code goes here
+  add_column :ponds, :name, :string 
+  add_column :ponds, :water_type, :string
 end
